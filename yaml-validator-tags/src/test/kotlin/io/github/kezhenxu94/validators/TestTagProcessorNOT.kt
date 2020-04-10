7c7e1bcd1626598fc1a7ebd4cbbad9caeb66bc03
kezhenxu94 <kezhenxu94@163.com>
2020-04-10 19:34:24 +0800

    Modify license to organization

--- yaml-validator-tags/src/test/kotlin/io/github/kezhenxu94/validators/TestTagProcessorNOT.kt
+++ yaml-validator-tags/src/test/kotlin/io/github/kezhenxu94/validators/TestTagProcessorNOT.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 kezhenxu94
+ * Copyright 2020 yaml-validator
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
